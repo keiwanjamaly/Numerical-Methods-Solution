f9c031ef275184f22d21dcaa3e0a5b2aa22983be
Keiwan Jamaly <keiwanjamaly@gmail.com>
2022-05-15 23:16:34 +0200

    fixed lecture 4

--- sheet_4/main.py
+++ sheet_4/main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.integrate import quad
 
 
 def exercise_1():
@@ -11,7 +12,7 @@ def exercise_1():
         return np.sin(x)**(2*m-1) * np.cos(x)**(2*n-1)
 
     result_tapezoidal = []
-    N_conv = np.arange(3, 30, 2, dtype=int)
+    N_conv = np.arange(5, 1000, 2, dtype=int)
     for N in N_conv:
         result = tapezoidal(lambda x: f(x, 2, 1), 0, np.pi/2, N)
         result_tapezoidal.append(result)
@@ -36,23 +37,22 @@ def exercise_2():
     from open_quadratures import tapezoidal, simpson
 
     # define the function to be integrated
-    def f(x, p):
-        return x**(p-1)/(1+x)
+    def f(y, p):
+        return (y/(1-y))**(p-1) / ((1.0 + y/(1-y)) * (1-y)**2)
 
-    x = 1000
-    print(tapezoidal(lambda x: f(x, 0.5), 0, x, x * 100),
-          tapezoidal(lambda x: f(x, 0.5), 0, 2*x, 2*x * 100))
+    def f(y, p):
+        return (np.tan(np.pi/2*y))**(p-1) / (1 + np.tan(np.pi/2*y)) * np.pi/(2 * np.cos(np.pi/2*y)**2)
 
     # evaluate integral at different sampling rates
     result_tapezoidal = []
-    N_conv = np.arange(9, 30000, 100, dtype=int)
+    N_conv = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
     for N in N_conv:
-        result = tapezoidal(lambda x: f(x, 0.5), 0, x, N)
+        result = tapezoidal(lambda pos: f(pos, 0.5), 0, 1, N)
         result_tapezoidal.append(result)
 
     result_simpson = []
     for N in N_conv:
-        result = simpson(lambda x: f(x, 0.5), 0, x, N)
+        result = simpson(lambda x: f(x, 0.5), 0, 1, N)
         result_simpson.append(result)
 
     # plot two solutions
@@ -60,6 +60,7 @@ def exercise_2():
     plt.plot(N_conv, result_simpson, label="simpson")
     plt.legend()
     plt.xlabel("N")
+    plt.xscale("log")
     plt.ylabel("$I_1$")
     plt.show()
 

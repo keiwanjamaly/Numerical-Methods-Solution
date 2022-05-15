6f80ba0c3cbe5e8674b04ce8fd293b2c5951e6ac
Keiwan Jamaly <keiwanjamaly@gmail.com>
2022-05-16 00:03:39 +0200

    added test's why different substitution functions work

---
+++ sheet_4/lecture 4 test.py
@@ -0,0 +1,43 @@
+from cProfile import label
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def logarithmic(x):
+    return -np.log(x)
+
+
+def logarithmic_inverse(x):
+    return -np.log(1-x)
+
+
+def tangential(x):
+    return np.tan(np.pi/2 * x)
+
+
+def quotential(x):
+    return x/(1-x)
+
+
+def function(x):
+    p = 0.5
+    return x**(p-1)/(1+x)
+
+
+if __name__ == "__main__":
+    x = np.linspace(0, 1, 10000000, endpoint=False)
+
+    # plt.plot(x, logarithmic(x), label="$-ln(x)$")
+    plt.plot(x, logarithmic_inverse(x), label="$-ln(x-1)$")
+    plt.plot(x, tangential(x), label="$\\tan(\pi/2x)$")
+    plt.plot(x, quotential(x), label="$x/(1-x)$")
+    # plt.plot(x, function(x), label="$f(x)$")
+
+    plt.legend()
+    plt.xlabel("x")
+    plt.ylabel("y(x)")
+
+    plt.xscale("log")
+    plt.yscale("log")
+
+    plt.show()

2d87993f18cf22e032c55eecf5720dd680f6a400
Keiwan Jamaly <keiwanjamaly@gmail.com>
2022-05-26 22:34:17 +0200

    modified solutions for sheet 4

--- sheet_4/lecture 4 test.py
+++ sheet_4/lecture 4 test.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     x = np.linspace(0, 1, 10000000, endpoint=False)
 
     # plt.plot(x, logarithmic(x), label="$-ln(x)$")
-    plt.plot(x, logarithmic_inverse(x), label="$-ln(x-1)$")
+    plt.plot(x, logarithmic_inverse(x), label="$-ln(1-x)$")
     plt.plot(x, tangential(x), label="$\\tan(\pi/2x)$")
     plt.plot(x, quotential(x), label="$x/(1-x)$")
     # plt.plot(x, function(x), label="$f(x)$")
--- sheet_4/main.py
+++ sheet_4/main.py
@@ -4,6 +4,8 @@ from scipy.integrate import quad
 
 
 def exercise_1():
+    m = 2
+    n = 4
     # import the necessary functions
     from closed_quadratures import tapezoidal, simpson
 
@@ -12,16 +14,19 @@ def exercise_1():
         return np.sin(x)**(2*m-1) * np.cos(x)**(2*n-1)
 
     result_tapezoidal = []
-    N_conv = np.arange(5, 1000, 2, dtype=int)
+    N_conv = np.arange(5, 100, 2, dtype=int)
     for N in N_conv:
-        result = tapezoidal(lambda x: f(x, 2, 1), 0, np.pi/2, N)
+        result = tapezoidal(lambda x: f(x, m, n), 0, np.pi/2, N)
         result_tapezoidal.append(result)
 
     # evaluate integral at different sampling rates
     result_simpson = []
     for N in N_conv:
-        result = simpson(lambda x: f(x, 2, 1), 0, np.pi/2, N)
+        result = simpson(lambda x: f(x, m, n), 0, np.pi/2, N)
         result_simpson.append(result)
+
+    print(
+        f"for N={N_conv[-1]}: I_trapezoid = {result_tapezoidal[-1]}, I_simpson={result_simpson[-1]}")
 
     # plot two solutions
     plt.plot(N_conv, result_tapezoidal, label="tapezoidal")
@@ -40,20 +45,20 @@ def exercise_2():
     def f(y, p):
         return (y/(1-y))**(p-1) / ((1.0 + y/(1-y)) * (1-y)**2)
 
-    def f(y, p):
-        return (np.tan(np.pi/2*y))**(p-1) / (1 + np.tan(np.pi/2*y)) * np.pi/(2 * np.cos(np.pi/2*y)**2)
+    # def f(y, p):
+    #     return (np.tan(np.pi/2*y))**(p-1) / (1 + np.tan(np.pi/2*y)) * np.pi/(2 * np.cos(np.pi/2*y)**2)
 
     # evaluate integral at different sampling rates
     result_tapezoidal = []
     N_conv = [10, 100, 1000, 10000, 100000, 1000000, 10000000]
     for N in N_conv:
         result = tapezoidal(lambda pos: f(pos, 0.5), 0, 1, N)
-        result_tapezoidal.append(result)
+        result_tapezoidal.append(result - np.pi)
 
     result_simpson = []
     for N in N_conv:
         result = simpson(lambda x: f(x, 0.5), 0, 1, N)
-        result_simpson.append(result)
+        result_simpson.append(result - np.pi)
 
     # plot two solutions
     plt.plot(N_conv, result_tapezoidal, label="tapezoidal")
@@ -61,10 +66,11 @@ def exercise_2():
     plt.legend()
     plt.xlabel("N")
     plt.xscale("log")
+    plt.yscale("symlog")
     plt.ylabel("$I_1$")
     plt.show()
 
 
 if __name__ == "__main__":
-    # exercise_1()
+    exercise_1()
     exercise_2()
